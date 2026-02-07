eddf7ef5c1e2e1029a59e88dc3faa438f3e12d60
turacicek68-spec <turacicek68@gmail.com>
2026-02-07 12:52:29 +0300

    ekleme

--- static/script.js
+++ static/script.js
@@ -1,4 +1,8 @@
+// script.js
 function downloadJarvis(){
-    alert("İndirme başlatıldı!"); 
-    window.location.href="/static/jarvis_setup.exe";
+    // Kullanıcıya kurulum klasörü hakkında önemli bir uyarı gösteriyoruz
+    alert("Tuğra Asistan İndiriliyor... \n\nÖNEMLİ: 'Vosk bulunamadı' hatası almamak için kurulum sırasında hedef klasör olarak 'C:\\Program Files\\TugraAsistan' klasörünü seçtiğinizden emin olun!"); 
+    
+    // Google Drive doğrudan indirme linki
+    window.location.href="https://drive.google.com/uc?export=download&id=1RRk8nM8U2Z23NsFFsPBesqWwZTdEvCOS";
 }
